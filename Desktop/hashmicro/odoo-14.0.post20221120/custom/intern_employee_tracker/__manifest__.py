fdf5570b1e063d0446302beb57171a25a6d757b3
ShammyJindan <98307898+ShammyJindan@users.noreply.github.com>
2026-03-09 11:34:03 +0700

    test push

---
+++ Desktop/hashmicro/odoo-14.0.post20221120/custom/intern_employee_tracker/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Intern Employee Tracker',
+    'version': '1.0',
+    'summary': 'Modul untuk tracking karyawan (Buggy Version)',
+    'category': 'Human Resources',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/employee_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ Desktop/hashmicro/odoo-14.0.post20221120/custom/intern_employee_tracker/models/employee.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api
+
+class InternEmployee(models.Model):
+    _name = 'intern.employee'
+    _description = 'Intern Employee Tracker'
+
+    name = fields.Char(string='Nama Karyawan', required=True)
+    position = fields.Char(string='Posisi')
+    base_salary = fields.Float(string='Gaji Pokok')
+    bonus = fields.Float(string='Bonus (10%)', compute='_compute_bonus')
+
+    @api.depends('base_salary')
+    def _compute_bonus(self):
+        self.bonus = self.base_salary * 0.1
